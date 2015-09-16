280da15141443f4bbb2b5eadbb5ce97e979fdd47
sunny2601 <sunnysarrot@gmail.com>
2015-09-17 01:50:52 +0530

    Update install_generator.rb

--- lib/generators/spree_pos/install/install_generator.rb
+++ lib/generators/spree_pos/install/install_generator.rb
@@ -3,14 +3,10 @@ module SpreePos
     class InstallGenerator < Rails::Generators::Base
 
       def add_javascripts
-        #append_file "app/assets/javascripts/store/all.js", "//= require store/spree_pos\n"
-        #append_file "app/assets/javascripts/admin/all.js", "//= require admin/spree_pos\n"
-      end
+       end
 
       def add_stylesheets
-        #inject_into_file "app/assets/stylesheets/store/all.css", " *= require store/spree_pos\n", :before => /\*\//, :verbose => true
-        #inject_into_file "app/assets/stylesheets/admin/all.css", " *= require admin/spree_pos\n", :before => /\*\//, :verbose => true
-      end
+       end
 
       def add_seed
         append_file "db/seeds.rb", <<-SEEDS
